e68458061e109b5edd3fd5691ec784d62e991a44
kylealanr <kylealanr@gmail.com>
2014-09-04 17:39:34 -0500

    *added NavigationDrawer package *added Fragments package *added SystemBarTint library *finished xml layout for MainActivity *changed ActionBar color in styles.xml *added navigation bar transparency for v19 *created drawer_item layout *added colors.xml file with all material colors

---
+++ mobile/src/main/java/com/itpos/itposcheckin/Fragments/TimeClock.java
@@ -0,0 +1,8 @@
+package com.itpos.itposcheckin.Fragments;
+
+/**
+ * Created by kylealanr on 9/4/14.
+ */
+public class TimeClock {
+
+}
---
+++ mobile/src/main/java/com/itpos/itposcheckin/Fragments/ToDo.java
@@ -0,0 +1,7 @@
+package com.itpos.itposcheckin.Fragments;
+
+/**
+ * Created by kylealanr on 9/4/14.
+ */
+public class ToDo {
+}
