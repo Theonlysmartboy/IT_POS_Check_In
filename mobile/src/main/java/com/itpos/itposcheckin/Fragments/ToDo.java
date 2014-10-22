db0435761092e1ee0baa30253acabb4165ba09ed
Kyle Riedemann <kylealanr@gmail.com>
2014-10-22 18:03:36 -0500

    *extend DefaultFragment from Todo fragment

--- mobile/src/main/java/com/itpos/itposcheckin/Fragments/ToDo.java
+++ mobile/src/main/java/com/itpos/itposcheckin/Fragments/ToDo.java
@@ -1,6 +1,5 @@
 package com.itpos.itposcheckin.Fragments;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,11 +10,17 @@ import com.itpos.itposcheckin.R;
 /**
  * Created by kylealanr on 9/4/14.
  */
-public class ToDo extends Fragment {
+public class ToDo extends DefaultFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.todo, container, false);
         return view;
     }
+
+    @Override
+    public void onBackStackChanged() {
+
+    }
+
     //TODO parse JSON returned from http request and add them to list
 }
