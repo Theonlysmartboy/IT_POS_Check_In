2c29cbbc6553b0e49f825803a5f55ad64a361209
Kyle Riedemann <kylealanr@gmail.com>
2014-10-22 16:38:53 -0500

    *added comments to make the purpose of the class clearer *handled a few more events and added some methods

--- mobile/src/main/java/com/itpos/itposcheckin/NavigationDrawer/NavigationDrawerAdapter.java
+++ mobile/src/main/java/com/itpos/itposcheckin/NavigationDrawer/NavigationDrawerAdapter.java
@@ -21,39 +21,92 @@ public class NavigationDrawerAdapter extends BaseAdapter {
     private Context context;
     private ArrayList<NavigationDrawerItem> navigationDrawerItems;
 
+    // default constructor
+    public NavigationDrawerAdapter(){
+    }
+
+    /**
+     *
+     * @param context
+     *      The context in which the NavDrawer is being constructed
+     * @param navigationDrawerItems
+     *      The ArrayList containing the DrawerItems for the Adapter
+     *
+     */
     public NavigationDrawerAdapter(Context context, ArrayList<NavigationDrawerItem> navigationDrawerItems){
         this.context = context;
         this.navigationDrawerItems = navigationDrawerItems;
     }
 
+    public Context getContext() { return context; }
+
+    public void setContext(Context context) { this.context = context; }
+
+    public ArrayList<NavigationDrawerItem> getNavigationDrawerItems() { return navigationDrawerItems; }
+
+    public void setNavigationDrawerItems(ArrayList<NavigationDrawerItem> navigationDrawerItems) {
+        this.navigationDrawerItems = navigationDrawerItems;
+    }
+
+    /**
+     * Used by the framework
+     *
+     * @return
+     *      The number of elements in this adapter
+     */
     @Override
     public int getCount() {
         return navigationDrawerItems.size();
     }
 
+    /**
+     * Used by onItemClickListener
+     *
+     * @param position
+     *      Poition of clicked item
+     * @return
+     *      The object in that position
+     */
     @Override
     public Object getItem(int position) {
         return navigationDrawerItems.get(position);
     }
 
+    /**
+     *
+     * @param position
+     *      Position of the item
+     * @return
+     *      The id of the item at that postiion
+     */
     @Override
     public long getItemId(int position) {
         return position;
     }
 
+    /**
+     * responsible for inflating and creating the View for each of the items
+     * To get different behavior for the different items in a List you can do so on getView
+     * EX:
+     * if(position % 2 == 0) {
+     *     convertView.setBackgroundColor(Color.BLUE);
+     * } else {
+     *     convertView.setBackgroundColor(Color.RED);
+     * }
+     */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
             LayoutInflater myInflater = (LayoutInflater)context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
             convertView = myInflater.inflate(R.layout.drawer_item, null);
         }
-
-        ImageView myIcon = (ImageView) convertView.findViewById(R.id.drawer_icon);
-        TextView myText = (TextView) convertView.findViewById(R.id.drawer_text);
-
-        myIcon.setImageResource(navigationDrawerItems.get(position).getIcon());
-        myText.setText(navigationDrawerItems.get(position).getTitle());
-
+        //create the image icon and title
+        ImageView iconImage = (ImageView) convertView.findViewById(R.id.drawer_icon);
+        TextView title = (TextView) convertView.findViewById(R.id.drawer_text);
+        //apply the specified properties to them
+        iconImage.setImageResource(navigationDrawerItems.get(position).getIcon());
+        title.setText(navigationDrawerItems.get(position).getTitle());
+        //return the created view
         return convertView;
     }
 }
