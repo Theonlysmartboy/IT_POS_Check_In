b8171895f4fbaf6c698f763544fee4359f740220
Kyle Riedemann <kylealanr@gmail.com>
2014-10-22 18:03:49 -0500

    *extend DefaultFragment from Time Clock fragment

--- mobile/src/main/java/com/itpos/itposcheckin/Fragments/TimeClock.java
+++ mobile/src/main/java/com/itpos/itposcheckin/Fragments/TimeClock.java
@@ -1,6 +1,5 @@
 package com.itpos.itposcheckin.Fragments;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,11 +10,17 @@ import com.itpos.itposcheckin.R;
 /**
  * Created by kylealanr on 9/4/14.
  */
-public class TimeClock extends Fragment {
+public class TimeClock extends DefaultFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.time_clock, container, false);
         return view;
     }
+
+    @Override
+    public void onBackStackChanged() {
+
+    }
+
     //TODO onClick make http request and get JSON
 }
